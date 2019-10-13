cf8917672fd1a83da665334359c0cb0fbadcfc0c
Mario <myeveryheart@qq.com>
2019-10-13 17:54:36 +0800

    跑起来

--- navigation/src/main.js
+++ navigation/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import { Button } from 'ant-design-vue';
+import App from './App'
+
+Vue.component(Button.name, Button)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
