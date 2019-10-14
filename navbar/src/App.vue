46beccb5d6de7255770f80f35dd277b8119a8adb
Mario <myeveryheart@qq.com>
2019-10-14 11:48:34 +0800

    报错

--- navbar/src/App.vue
+++ navbar/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <a-menu v-model="current" mode="horizontal" theme="dark">
-      <a-sub-menu>
-        <span slot="title" class="submenu-title-wrapper">
-          <a-icon type="setting" />Navigation
-        </span>
-        <a-menu-item key="setting:1">打开App1</a-menu-item>
-        <a-menu-item key="setting:2">打开App2</a-menu-item>
-      </a-sub-menu>
-    </a-menu>
+    <div>
+      <a-menu v-model="current" mode="horizontal" theme="dark">
+        <a-sub-menu>
+          <span slot="title" class="submenu-title-wrapper">
+            <a-icon type="setting" />Navigation
+          </span>
+          <a-menu-item key="setting:1" @click="goApp1">打开App1</a-menu-item>
+          <a-menu-item key="setting:2" @click="goApp2">打开App2</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 <script>
@@ -17,6 +20,12 @@ export default {
     return {
       current: ["mail"]
     };
+  },
+  methods: {
+    goApp1() {
+      console.log("111");
+    },
+    goApp2() {}
   }
 };
 </script>
